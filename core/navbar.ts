f0b4c66553838b267daa7ccb3cd234d1858fc6f6
Juan <juan@shmugo.co>
2023-11-06 14:43:25 -0500

    Add Secondary Derivates to Index NavBart
    TODO: Add Support for Other Derivates

--- core/navbar.ts
+++ core/navbar.ts
@@ -22,14 +22,16 @@ export async function processNavBar(
         setTimeout(async function () {
             for (const file of queue) {
                 // Initiate Key based off PATH
-                let path: string = queue[1];
+                let path: string = file[0];
                 if (path == '.') {
                     path = 'index';
                 } else {
                     path = path.match(/^(\w+)\\\\|^(\w+)/g)![0]; // TODO: grab error here
                 }
-                // Initiate SubPath Value
-                buff[path] = '<nav>';
+                // Initiate SubPath Value if empty
+                if (!buff[path]) {
+                    buff[path] = '<nav>';
+                }
 
                 // Read the first line of the Markdown file to extract the title.
                 // const readable = fs.createReadStream(`${src}//${file}`);
@@ -42,18 +44,49 @@ export async function processNavBar(
                 // readable.close();
 
                 // Extract the file name and extension from the filename.
-                let fileRegEx: any = String(file).match(/([^\\]*)\.(\w+)$/);
+                let fileRegEx: any = String(file[1]).match(/([^\\]*)\.(\w+)$/);
                 let fileName = fileRegEx[1];
 
                 // Construct a navigation link for the Markdown file using its title.
                 // Goes through each iteration
                 buff[
                     path
-                ] += `<a href="${queue[1]}\\${fileName}.html"> ${fileName}</a> |\n`;
+                ] += `<a href="${file[0]}\\${fileName}.html"> ${fileName}</a> `;
             }
-            // Close <nav> initialization per key
+
+            // Add Secondary Page to Primary NavBar
             for (const i in buff) {
+                // Add Main File of Secondary Section to NavBar
+                if (i == 'index') {
+                    for (const file of queue) {
+                        // Initiate Key based off PATH
+                        let path: string = file[0];
+                        if (path == '.' || path.includes('\\')) {
+                            continue;
+                        } else {
+                            path = path.match(/^(\w+)\\\\|^(\w+)/g)![0]; // TODO: grab error here
+                        }
+
+                        let fileRegEx: any = String(file[1]).match(
+                            /([^\\]*)\.(\w+)$/
+                        );
+                        let fileName = fileRegEx[1];
+
+                        buff[
+                            'index' // for whatever reason this wouldn't work without specifying it is index
+                        ] += `<a href="${file[0]}\\${fileName}.html"> ${fileName}</a> `;
+                    }
+                }
+
+                // Close <nav> initialization per key
                 buff[i] += '</nav>';
+            }
+            // Add Index NavBar to Secondary NavBar
+            // TODO: Add Support for NavBars below Secondary
+            for (const i in buff) {
+                if (i != 'index') {
+                    buff[i] = buff['index'] + buff[i];
+                }
             }
 
             // Resolve the Promise with the generated navigation bar string.
